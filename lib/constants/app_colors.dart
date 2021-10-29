84e55b9e3af069f67d339ea59fbfa9ba443d68fc
Fady Fawzy <FadyFawzy.Official@gmail.com>
2021-10-29 17:35:27 +0200

    6. Add the Constants Colors the App will use

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Color(0xFFE1E8EB);
+  static const Color grey = Color(0xFF343A40);
+  static const Color yellow = Color(0xFFFFC107);
+}
