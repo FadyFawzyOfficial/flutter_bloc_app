75d4215daef3af65800810beb046d7b525fa5439
Fady Fawzy <FadyFawzy.Official@gmail.com>
2021-10-29 17:27:10 +0200

    5. Initialize CharacterDetailsScreen & Split the Constants

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_app/constants/strings.dart';
+import 'package:flutter_bloc_app/presentation/screens/character_details_screen.dart';
 
 import 'presentation/screens/characters_screen.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
-      case '/':
+      case charactersScreen:
         return MaterialPageRoute(builder: (_) => const CharactersScreen());
+      case characterDetailsScreen:
+        return MaterialPageRoute(
+            builder: (_) => const CharacterDetailsScreen());
     }
   }
 }
---
+++ lib/constants/strings.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_bloc_app/presentation/screens/character_details_screen.dart';
+
+/// Routes Names
+const charactersScreen = '/';
+const characterDetailsScreen = '/character_details';
