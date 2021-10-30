c53398fcff958ae6a90cf20237ebe86879745281
Fady Fawzy <FadyFawzy.Official@gmail.com>
2021-10-30 11:42:32 +0200

    2. Use BlocBuilder to Build CharactersScreen UI State

--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_app/business_logic/cubit/characters_cubit.dart';
+import 'package:flutter_bloc_app/constants/app_colors.dart';
 
-class CharactersScreen extends StatelessWidget {
+import '../../data/models/character.dart';
+
+class CharactersScreen extends StatefulWidget {
   const CharactersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  late List<Character> allCharacters;
+
+  @override
+  void initState() {
+    super.initState();
+
+    allCharacters = BlocProvider.of<CharactersCubit>(context).allCharacters;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(child: Text('Fady Fawzy')),
+      appBar: AppBar(
+        backgroundColor: AppColors.yellow,
+        title: const Text(
+          'Characters',
+          style: TextStyle(color: AppColors.grey),
+        ),
+      ),
+      body: BlocBuilder<CharactersCubit, CharactersState>(
+        builder: (context, state) {
+          if (state is CharactersLoaded) {
+            allCharacters = state.characters;
+            return GridView.builder(
+              itemCount: allCharacters.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 2 / 3,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+              ),
+              itemBuilder: (context, index) => Container(),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: AppColors.yellow,
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
