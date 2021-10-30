bc14ce371bbdc1aa05bcf01bf073d37b918b798f
Fady Fawzy <FadyFawzy.Official@gmail.com>
2021-10-30 10:33:09 +0200

    1. Use BlocProvider to Provide a Cubit (CharactersCubit) to a Screen (CharactersScreen)

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_app/constants/strings.dart';
-import 'package:flutter_bloc_app/presentation/screens/character_details_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'business_logic/cubit/characters_cubit.dart';
+import 'constants/strings.dart';
+import 'data/repositories/characters_repository.dart';
+import 'data/services/character_service.dart';
+import 'presentation/screens/character_details_screen.dart';
 import 'presentation/screens/characters_screen.dart';
 
 class AppRouter {
+  late CharactersRepository charactersRepository;
+  late CharactersCubit charactersCubit;
+
+  AppRouter() {
+    charactersRepository = CharactersRepository(CharacterService());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
+
   Route? generateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case charactersScreen:
-        return MaterialPageRoute(builder: (_) => const CharactersScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => charactersCubit,
+            child: const CharactersScreen(),
+          ),
+        );
       case characterDetailsScreen:
         return MaterialPageRoute(
             builder: (_) => const CharacterDetailsScreen());
